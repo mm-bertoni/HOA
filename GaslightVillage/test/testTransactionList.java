f12303696b1c26ad77916218008576d59f8cad68
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-06-25 10:55:41 -0400

    Placeholders for test classes

---
+++ GaslightVillage/test/testTransactionList.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class testTransactionList {
+  @Test
+  public void testCalculateExpectedDeposits(){}
+
+  @Test
+  public void testCalculateExpectedExpenses(){}
+
+  @Test
+  public void testAddTransaction(){}
+
+
+
+}
