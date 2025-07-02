199c8376d7ea3d7a2af745d990390764a2b2f56d
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-07-02 12:34:57 -0400

    Tests for Project and ProjectList classes

--- GaslightVillage/test/testProjectList.java
+++ GaslightVillage/test/testProjectList.java
@@ -1,8 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import Helpers.Category;
+import Helpers.Status;
+
 
 public class testProjectList {
   ProjectList projectList = new ProjectList();
-  Project project = new Project("Reinforcements", Category.Basement);
+  Project testProject = new Project("Reinforcements", Category.Basement);
+
+
+  @Test
+  public void testAddGetProject(){
+    projectList.addProject(testProject);
+    testProject.setApprovalStatus(Status.Approved);
+    Assertions.assertEquals(projectList.getProjects(Status.Approved), testProject);
+  }
 
 
 }
